2cdb1b94e4f9f6ab46ee6611a2b95193503088b1
Luis Velasquez <lvelasquez17903@gmail.com>
2022-11-25 20:14:39 -0300

    Primer commit

---
+++ app/src/main/java/com/example/evaluacion_2_iot_madariaga_velasquez/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.evaluacion_2_iot_madariaga_velasquez;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
